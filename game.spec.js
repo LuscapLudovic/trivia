8480fa983c9a1c4a60a6587c70f2d064ed126f0f
MrPopiette <maxime.monnier@epsi.fr>
2021-04-08 13:50:38 +0200

    unit tests + packagejson

--- game.spec.js
+++ game.spec.js
@@ -1,15 +1,60 @@
 require('./game.js');
 
+/**
+ * Test pour voir si les tests se lancent correctement
+ */
 describe("The test environment", function() {
     it("should pass", function() {
         expect(true).toBe(true);
     });
-
-    it("should access game", function() {
-        expect(Game).toBeDefined();
-    });
 });
 
-describe("Your specs...", function() {
-    // it ...
+/**
+ * Test de creation d'un jeu
+ * @param Boolean true
+ */
+describe("Game creation", function(techno = true) {
+    game = new Game(techno)
+
+    it("should create a game with techno questions ", function() {
+        expect(game).toBeDefined();
+    })
+});
+
+/**
+ * Test de creation d'un jeu
+ * @param Boolean false
+ */
+describe("Game creation", function(techno = false) {
+    game = new Game(techno)
+
+    it("should create a game with rock questions ", function() {
+        expect(game).toBeDefined();
+    })
+});
+
+
+
+
+/**
+ * Test de creation d'un joueur avec un nom
+ *  * @param String name
+ */
+describe("Player creation", function(name) {
+    jackie = new Player(name)
+
+    it("should create a player named ", function() {
+        expect(jackie.name).toBe(name);
+    })
+});
+
+/**
+ * Test de creation d'un joueur sans nom
+ */
+describe("Player creation", function() {
+    jackie = new Player()
+
+    it("should create a player not named ", function() {
+        expect(jackie.name).toBeUndefined();
+    })
 });
